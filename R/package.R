7369d61d2f9bbd18432beab5f47759ff7ef86b77
Simon Wotherspoon <Simon.Wotherspoon@aad.gov.au>
2022-10-09 23:46:37 +1100

    Initial import
    Initial import of code developed with BR in 2017,

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' Flat smooths
+#'
+#' Provides facilities for estimating broadscale animal motions from
+#' local light level data recorded by an archival tag.
+#'
+#' @name flat-package
+#' @docType package
+#' @author S. Wotherspoon and B. Raymond
+NULL
