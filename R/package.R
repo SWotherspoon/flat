712892225daa83134ad08cc6162e3de581f732a3
Simon Wotherspoon <Simon.Wotherspoon@aad.gov.au>
2025-04-13 13:30:07 +1000

    Package modernization
    Updated documentation

--- R/package.R
+++
@@ -1,9 +0,0 @@
-#' Flat smooths
-#'
-#' Provides facilities for estimating broadscale animal motions from
-#' local light level data recorded by an archival tag.
-#'
-#' @name flat-package
-#' @docType package
-#' @author S. Wotherspoon and B. Raymond
-NULL
